ee3133b9d9b017009a4f89c725e5df2960c9159a
joaocarneiro1 <56590523+joaocarneiro1@users.noreply.github.com>
2020-06-12 14:40:35 +0100

    Add files via upload

---
+++ Sem2.js
@@ -0,0 +1,62 @@
+----------------------------
+			If
+----------------------------
+if (condição) {
+    // Bloco de código para ser executado.
+}
+
+if (x < 5) {
+    mensagem = "É menor que 5";
+}
+----------------------------
+if (condicao) {
+    //Se coincidir executa o bloco de codigo aqui posto.
+} else {
+    //Senão coincidir executa o bloco de codigo aqui posto.
+}
+
+if (x< 5) {
+    mensagem = "É menor que 5";
+} else {
+    mensagem = "É maior que 5";
+}
+----------------------------
+
+if (condicao-1) {
+    // Será executado este bloco caso a condição 1 seja verdadeira
+} else if (condicao-1) {
+    // Será executado este bloco caso a condição 1 seja falsa, e a 2 seja verdadeira
+} else {
+    // Será executado este bloco caso as condições 1 e 2 sejam falsas
+}
+
+if (x< 5) {
+    mensagem = "É menor que 5";
+} else if (x > 5){
+    mensagem = "É maior que 5";
+} else {
+   mensagem = "É igual a 5";
+}
+----------------------------
+			For
+----------------------------
+
+for (i = 0; i < 9; i++)
+{
+  ex = ex + i;
+}
+
+----------------------------
+ 			Switch
+----------------------------			
+switch(expressao) {
+    case a:
+        // bloco de código
+        break;
+    case b:
+        // bloco de código
+        break;
+    default:
+        // bloco de block
+}
+
